f648b016683bbce240accf0f2e0cdf5e0d0478f6
Daniel Ramirez <danielramirezduque1@gmail.com>
2024-09-23 09:34:12 -0500

    Add adminMiddleware

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,5 +1,16 @@
 // middlewares/adminMiddleware.js
-
-function adminMiddleware(req, res, next) {}
+const users = require('../data/users.json')
+function adminMiddleware(req, res, next) {
+  const { email } = req.body
+  const user = users.find((u) => u.email === email)
+  // Redirigir según el rol del usuario
+  if (user.rol === 'admin') {
+    return res.redirect('/admin')
+  } else if (user.rol === 'client') {
+    return res.redirect('/')
+  }
+  // Si por alguna razón el rol no es ni admin ni client, simplemente avanza
+  next()
+}
 
 module.exports = adminMiddleware
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const UserController = require('../controllers/UserController')
+const adminMiddleware = require('../middlewares/adminMiddleware')
 const router = express.Router()
 
 // Renderizar el formulario de login
 router.get('/login', UserController.login)
 
 // Procesar el login
-router.post('/login', UserController.processLogin)
+router.post('/login', adminMiddleware, UserController.processLogin)
 
 module.exports = router
