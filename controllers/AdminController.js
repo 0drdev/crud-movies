3369f041d0905d319cfddb06999509aaefffc99a
Daniel Ramirez <danielramirezduque1@gmail.com>
2024-09-25 18:26:33 -0500

    Add methods create, edit movie in the administrator

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,3 +1,7 @@
+const fs = require('node:fs')
+const path = require('node:path')
+const movies = require('../data/movies.json')
+
 const AdminController = {
   index: (req, res) => {
     res.render('admin/admin', {
@@ -5,6 +9,95 @@ const AdminController = {
       message: 'Bienvenido al panel de administración',
       user: req.user /// / El usuario ya está disponible gracias al middleware
     })
+  },
+  allMovies: (req, res) => {
+    const data = fs.readFileSync(path.join('data', 'movies.json'), 'utf8')
+    const movies = JSON.parse(data)
+    res.render('admin/admin-movies', { movies, title: 'Movies' })
+  },
+  createMovie: (req, res) => {
+    res.render('admin/create-movie', { title: 'Crear movie' })
+  },
+  storeMovie: (req, res) => {
+    // Verificar que los datos lleguen al servidor
+    console.log('Datos recibidos:', req.body)
+
+    // Leer las películas actuales
+    const data = fs.readFileSync(
+      path.join(__dirname, '../data/movies.json'),
+      'utf8'
+    )
+    const movies = JSON.parse(data)
+
+    // Crear una nueva película con los datos recibidos
+    const newMovie = {
+      id: movies.length + 1, // Generar un ID único
+      title: req.body.title || 'Título desconocido', // Asignar valores por defecto si falta algún campo
+      year: req.body.year || 'Año no especificado',
+      director: req.body.director || 'Director no especificado',
+      duration: req.body.duration || 'Duración no especificada',
+      genre: req.body.genre || 'Género no especificado',
+      rate: req.body.rate || '0',
+      poster: req.body.poster || 'URL no especificada'
+    }
+
+    // Verificar los valores de la nueva película
+    console.log('Nueva película:', newMovie)
+
+    // Agregar la nueva película al array de movies
+    movies.push(newMovie)
+
+    // Guardar los cambios en el archivo movies.json
+    fs.writeFileSync(
+      path.join(__dirname, '../data/movies.json'),
+      JSON.stringify(movies, null, 2),
+      'utf8'
+    )
+
+    // Redirigir a la lista de películas después de crear la nueva película
+    res.redirect('/admin/movies')
+  },
+  showEditMovie: (req, res) => {
+    const movie = movies.find((movie) => movie.id === req.params.id)
+    if (movie) {
+      res.render('admin/edit-movie', { movies, movie, title: 'Edit movie' })
+    } else {
+      res.status(404).send('Movie not found')
+    }
+  },
+  editMovie: (req, res) => {
+    // Encontrar el índice de la película
+    const movieIndex = movies.findIndex((movie) => movie.id === req.params.id)
+
+    if (movieIndex !== -1) {
+      // Actualizar la película con los datos recibidos
+      movies[movieIndex] = {
+        ...movies[movieIndex],
+        title: req.body.title,
+        year: req.body.year,
+        director: req.body.director,
+        duration: req.body.duration,
+        poster: req.body.poster,
+        genre: req.body.genre,
+        rate: req.body.rate
+      }
+
+      // Escribir los cambios en el archivo movies.json
+      fs.writeFileSync(
+        path.join('data', 'movies.json'),
+        JSON.stringify(movies, null, 2),
+        'utf8'
+      )
+
+      // Redirigir a la página de películas después de guardar los cambios
+      res.redirect('/admin/movies')
+    } else {
+      res.status(404).send('Movie not found')
+    }
+  },
+  deleteMovie: (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
   }
 }
 
--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,16 @@ const router = express.Router()
 
 router.get('/', adminMiddleware, AdminController.index)
 
+router.get('/movies', adminMiddleware, AdminController.allMovies)
+
+router.get('/movies/create', adminMiddleware, AdminController.createMovie)
+
+router.post('/movies/create', adminMiddleware, AdminController.storeMovie)
+
+router.get('/movies/edit/:id', adminMiddleware, AdminController.showEditMovie)
+
+router.post('/movies/edit/:id', adminMiddleware, AdminController.editMovie)
+
+router.get('/movies/delete/:id', adminMiddleware, AdminController.deleteMovie)
+
 module.exports = router
